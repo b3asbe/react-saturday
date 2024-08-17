1396a3b5fb1007dfcd18d2fe57ee4cad2a0c5771
b3asbe <168250840+b3asbe@users.noreply.github.com>
2024-08-17 22:55:39 +0000

    He creado un compomente llamado Mensaje

---
+++ AppReact/src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import Mensaje from "./componente/Mensaje"
+
+function App() {
+
+  return (
+    <>
+      <Mensaje />
+      <Mensaje />
+      <Mensaje />
+    </>
+  )
+}
+
+export default App;
---
+++ AppReact/src/componente/Mensaje.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+function Mensaje()
+{
+
+    return (
+        <>
+            <h1>Ricky,</h1>
+            <h1>Anthony,</h1>
+        </>)
+
+        ;
+}
+
+export default Mensaje;
